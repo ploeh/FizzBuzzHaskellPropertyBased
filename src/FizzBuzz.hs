459383a4b8c440c7141a99d6be477b3f1b450b94
Mark Seemann <mark@ploeh.dk>
2021-06-19 10:02:32 +0200

    Add first property
    If things go according to plan, this will be the only property that uses
    partition testing.

--- src/FizzBuzz.hs
+++ src/FizzBuzz.hs
@@ -1,1 +1,4 @@
 module FizzBuzz where
+
+fizzBuzz :: Integral a => a -> String
+fizzBuzz _ = "FizzBuzz"
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,4 +1,17 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 module Main where
 
+import FizzBuzz
+import Test.Framework
+import Test.Framework.Providers.QuickCheck2
+import Test.QuickCheck
+
 main :: IO ()
-main = return ()
+main =
+  defaultMain
+  [
+    testProperty "Divisible by both 3 and 5" $ \ (seed :: Int) ->
+      let i = seed * 3 * 5
+          actual = fizzBuzz i
+      in "FizzBuzz" === actual
+  ]
